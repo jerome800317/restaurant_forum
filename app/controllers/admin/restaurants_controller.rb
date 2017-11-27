aebbba984d3f81283a9ddfbc1589b9a470100e14
Jerome Lai <jerome800317@hotmail.com>
2017-11-27 22:44:50 +0800

    set up admin authenticate

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,4 +1,7 @@
 class Admin::RestaurantsController < ApplicationController
+  #在 before_action呼叫驗證方法
+  before_action :authenticate_user!
+  before_action :authenticate_admin
 
   def index
     
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  def admin?
+    self.role == "admin"
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
