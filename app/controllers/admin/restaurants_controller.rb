fd8a72c74ae130b8aa0d038a21feee589dba5611
Jerome Lai <jerome800317@hotmail.com>
2017-11-28 23:55:25 +0800

    add restaurant destroy  action for admin

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -2,7 +2,7 @@ class Admin::RestaurantsController < ApplicationController
   #在 before_action呼叫驗證方法
   before_action :authenticate_user!
   before_action :authenticate_admin
-  before_action :set_restaurant, only: [:show, :edit, :update]
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
 
   def index
     @restaurants = Restaurant.all
@@ -31,6 +31,12 @@ class Admin::RestaurantsController < ApplicationController
       flash.now[:alert] = "restaurant was failed to create, you need to add colume of name"
       render :edit
     end
+  end
+
+  def destroy
+    @restaurant.destroy
+    flash[:alert] = "restaurant was deleted"
+    redirect_to admin_restaurants_path
   end
 
   private
