49e715393811c195f11edbc9e9b65415487d1e70
Jerome Lai <jerome800317@hotmail.com>
2017-12-11 22:49:11 +0800

    move authenticate_user! to Application_Controller.rb

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,6 +1,5 @@
 class Admin::RestaurantsController < ApplicationController
   #在 before_action呼叫驗證方法
-  before_action :authenticate_user!
   before_action :authenticate_admin
   before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
 
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,5 @@
 class RestaurantsController < ApplicationController
 
-  before_action :authenticate_user!
-
   def index
     @restaurants = Restaurant.all
   end
