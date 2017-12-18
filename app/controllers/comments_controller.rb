fa757b75a1b64a1449dcda5f272508daea0e32ce
Jerome Lai <jerome800317@hotmail.com>
2017-12-18 18:27:44 +0800

    admin can delete comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,14 +2,24 @@ class CommentsController < ApplicationController
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
     @comment = @restaurant.comments.build(comment_params)
-    @comment_user = current_user
+    @comment.user = current_user
     @comment.save!
     redirect_to restaurant_path(@restaurant)
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @comment = Comment.find(params[:id])
+
+    if current_user.admin?
+      @comment.destroy
+      redirect_to restaurant_path(@restaurant)
+    end
   end
 
   private
 
   def comment_params
-    params.require(:comments).permit(:content)
+    params.require(:comment).permit(:content)
   end
 end
