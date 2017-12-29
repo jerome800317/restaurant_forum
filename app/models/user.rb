dacb9eed97290d2c492bcb27774b8647ddf59057
Jerome Lai <jerome800317@hotmail.com>
2017-12-29 22:44:31 +0800

    generate favorite model and setup relation among user, restaurant and favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,9 @@ class User < ApplicationRecord
 
   has_many :comments
   has_many :restaurants, through: :comments
+
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_restaurants, through: :favorites, source: :restaurant
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171220080339) do
+ActiveRecord::Schema.define(version: 20171229142142) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20171220080339) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
+    t.integer "user_id"
+    t.integer "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "restaurant_id"
     t.datetime "created_at", null: false
