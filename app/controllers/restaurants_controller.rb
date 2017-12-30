eb7acbe5ed67686312fd256c200ae13eab9d4af6
Jerome Lai <jerome800317@hotmail.com>
2017-12-30 14:37:49 +0800

    build favorite and unfavorite botton on restaurant show page and complete the setup of favorite and unfavorite action

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -18,4 +18,20 @@ class RestaurantsController < ApplicationController
   def dashboard
     @restaurant = Restaurant.find(params[:id])
   end
+
+  def favorite
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.favorites.create!(user: current_user)
+    #亦可用以下兩種方法建立favorites資料庫的資料
+    #Favorite.create(restaurant: @restaurant, user: current_user)
+    #current_user.favorites.create(restaurant: @restaurant
+    redirect_back(fallback_location: root_path)
+  end
+
+  def unfavorite
+    @restaurant = Restaurant.find(params[:id])
+    favorite = Favorite.where(restaurant: @restaurant, user: current_user)#由於favorite只在action中使用, 所以慣例不使用實例變數, 然而使用實例變數也不會影響結果
+    favorite.destroy_all #due to the data type is array so we add "all" in the end
+    redirect_back(fallback_location: root_path)
+  end
 end
