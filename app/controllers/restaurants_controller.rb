35434dab56525d53f7f430fb040baf3a5fb8fcdd
Jerome Lai <jerome800317@hotmail.com>
2018-01-03 00:18:39 +0800

    setup before_action to set restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,5 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :dashboard, :favorite, :unfavorite, :like, :unlike]
 
   def index
     @restaurants = Restaurant.page(params[:page]).per(9)
@@ -6,7 +7,6 @@ class RestaurantsController < ApplicationController
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])
     @comment = Comment.new
   end
 
@@ -15,12 +15,7 @@ class RestaurantsController < ApplicationController
     @recent_comments = Comment.order(created_at: :desc).limit(10)
   end
 
-  def dashboard
-    @restaurant = Restaurant.find(params[:id])
-  end
-
   def favorite
-    @restaurant = Restaurant.find(params[:id])
     @restaurant.favorites.create!(user: current_user)
     #亦可用以下兩種方法建立favorites資料庫的資料
     #Favorite.create(restaurant: @restaurant, user: current_user)
@@ -29,22 +24,25 @@ class RestaurantsController < ApplicationController
   end
 
   def unfavorite
-    @restaurant = Restaurant.find(params[:id])
     favorite = Favorite.where(restaurant: @restaurant, user: current_user)#由於favorite只在action中使用, 所以慣例不使用實例變數, 然而使用實例變數也不會影響結果
     favorite.destroy_all #due to the data type is array so we add "all" in the end
     redirect_back(fallback_location: root_path)
   end
 
   def like
-    @restaurant = Restaurant.find(params[:id])
     @restaurant.likes.create!(user: current_user)
     redirect_back(fallback_location: root_path)
   end
 
   def unlike
-    @restaurant = Restaurant.find(params[:id])
     like = Like.where(user: current_user, restaurant: @restaurant)
     like.destroy_all
     redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 end
