8e9d7ad142ded6d48d8f2f4970401c3a66112850
Jerome Lai <jerome800317@hotmail.com>
2018-01-05 22:54:54 +0800

    setup routes for user index and follow/unfollow function then generate controller followships

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   #For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :followships, only: [:create, :destroy]
+
   resources :restaurants, only: [:index, :show] do
     resources :comments, only:[:create, :destroy]
 
